3f3848fb1b7134d8f53c3dc4ba11b6da394ec42a
Chase Arrington <chasearrington1@gmail.com>
2016-03-22 01:01:16 -0500

    Changes in Problem 1

--- Problem 1/Problem 1.py
+++ Problem 1/Problem 1.py
@@ -43,18 +43,19 @@ print '\nselect * from s_dept: ', s_dept
 
 print '\nselect last_name, first_name, title, salary from s_emp: ', [[i[1], i[2], i[6], i[7]] for i in s_emp[1::]]
 
-
 print '\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40: ', [[i[1], i[2], i[6], i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40]
 
+print '\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by last_name: ', sorted([[i[1], i[2], i[6], i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40], key = lambda x: x[0])
 
-print '\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by last_name: '
+print '\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by salary desc: ', sorted([[i[1], i[2], i[6], i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40], key = lambda x: x[3], reverse = True)
 
-
-print '\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by salary desc: '
-
-
-print '\nselect last_name, first_name, title, salary, name from s_emp e join s_dept d on(e.dept_id = d.id): '
+print '\nselect last_name, first_name, title, salary, name from s_emp e join s_dept d on(e.dept_id = d.id): ', [[i[1], i[2], i[6], i[7], j[1]] for i in s_emp[1::] for j in s_dept[1::] if i[9] == j[0]]
 
 print '\nselect dept_id, avg(salary) from s_emp group by dept_id order by dept_id: '
+for department in { d[9] for d in s_emp[1::] }: print (department, (lambda l: round(sum(l) / len(l), 2))(map(float,[e[7] for e in s_emp[1::] if e[9] == department])))
+
+#[d[9] for d in s_emp[1::], (lambda l: round(sum(l) / len(l), 2))(map(float,[e[7] for e in s_emp[1::] if e[9] == d[9]]))]
+
 
 print '\nselect dept_id, avg(salary) from s_emp group by dept_id having avg(salary) < 1500;'
+for department in { d[9] for d in s_emp[1::] }: print (lambda dept_id, avgSal: (dept_id, avgSal) if avgSal <1500 else "")(department, (lambda l: round(sum(l) / len(l), 2))(map(float,[ e[7] for e in s_emp[1::] if e[9] == department ])))
